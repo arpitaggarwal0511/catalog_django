88db9254c0b4196f89762e3c10d593d0c0d23785
Arpit Aggarwal <arpitaggarwal0511@gmail.com>
2025-12-12 15:31:32 +0530

    views

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Products
 
-# Register your models here.
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ("name","price","rating","size","image_url")
+    search_fields = ("name","description")
+    list_filter = ("size","rating")
--- products/views.py
+++ products/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def product_list(request):
+    return HttpResponse("Products list placeholder")
 # Create your views here.
